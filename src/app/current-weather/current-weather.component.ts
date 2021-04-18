58b3d84c84dd59cfd4105f9dd4a7d2e29083288d
schultz <ambassador.life@gmail.com>
2021-04-18 12:47:13 +0700

    create current-weather component

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../interfaces';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.scss'],
+})
+export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather;
+
+  constructor() {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: new Date(),
+      image: 'assets/img/sunny.svg',
+      temperature: 72,
+      description: 'sunny',
+    };
+  }
+
+  ngOnInit(): void {}
+}
