2ab9efb839eb8910f36c508ec49e75f096da5ed0
schultz <ambassador.life@gmail.com>
2021-04-18 15:51:11 +0700

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'hochiminh', pathMatch: 'full' },
+  { path: ':location', component: CurrentWeatherComponent },
+];
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ICurrentWeather } from '../interfaces';
 import { WeatherService } from '../weather/weather.service';
+import { ActivatedRoute } from '@angular/router';
+
+interface IPlace {
+  city: string;
+  country: string;
+}
+
+type City = 'hochiminh' | 'hanoi' | 'quynhon';
 
 @Component({
   selector: 'app-current-weather',
@@ -9,12 +17,33 @@ import { WeatherService } from '../weather/weather.service';
 })
 export class CurrentWeatherComponent implements OnInit {
   current!: ICurrentWeather;
+  locations = {
+    hochiminh: {
+      city: 'Thanh pho Ho Chi Minh',
+      country: 'VN',
+    },
+    hanoi: {
+      city: 'Ha Noi',
+      country: 'VN',
+    },
+    quynhon: {
+      city: 'Quy Nhon',
+      country: 'VN',
+    },
+  };
+  place!: IPlace;
 
-  constructor(private weatherService: WeatherService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private weatherService: WeatherService
+  ) {}
 
   ngOnInit(): void {
-    this.weatherService
-      .getCurrentWeather('Thanh pho Ho Chi Minh', 'VN')
-      .subscribe((data) => (this.current = data));
+    this.route.params.subscribe((params) => {
+      this.place = this.locations[params.location as City];
+      this.weatherService
+        .getCurrentWeather(this.place.city, this.place.country)
+        .subscribe((data) => (this.current = data));
+    });
   }
 }
